1b064ddefb16380bf1a058dcf9df042adf8f1791
Thiago Giammattey <giammattey@gmail.com>
2020-06-27 08:46:43 -0300

    the log in action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,34 @@ class SessionsController < ApplicationController
     # because we're not adding to the database
   end
 
+  def create
+    @username = form_params[:username]
+    @password = form_params[:password]
+
+    @user = User.find_by(username: @username).try(:authenticate, @password)
+
+    if @user.present?
+      session[:user_id] = @user.id
+
+      flash[:success] = "You are now logged in!"
+
+      redirect_to root_path
+    else
+      render "new"
+    end
+
+  end
+
   def destroy
     reset_session
 
     flash[:success] = "You are now logged out!"
 
     redirect_to root_path
+  end
+
+  def form_params
+    params.require(:session).permit(:username, :password)
   end
 
 end
