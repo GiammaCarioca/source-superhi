9981968251d1ca33888a4560de1907d2407e398c
Thiago Giammattey <giammattey@gmail.com>
2020-06-27 08:32:32 -0300

    log out and log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+
+  def new
+    # we don't need any variables in here,
+    # because we're not adding to the database
+  end
+
+  def destroy
+    reset_session
+
+    flash[:success] = "You are now logged out!"
+
+    redirect_to root_path
+  end
+
 end
