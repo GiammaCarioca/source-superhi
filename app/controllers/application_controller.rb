bdbf8390bd20f84e30f0887c5ebf52fa0e59bc4c
Thiago Giammattey <giammattey@gmail.com>
2020-06-27 07:49:54 -0300

    the signup sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+  # make current_user available to views
+  helper_method :current_user
+
+  # run this on every single page and action
+  before_action :current_user
+
+  def current_user
+    if session[:user_id].present?
+      @current_user = User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+
+    # return
+    @current_user
+  end
+
 end
